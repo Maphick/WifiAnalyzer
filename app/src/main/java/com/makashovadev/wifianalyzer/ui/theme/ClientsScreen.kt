e780c53ee410e7504ac7ed86a87908cbe582e33c
mmb <mmb@gitlab.server.com>
2024-07-12 10:44:31 +0300

    LazyColumn was added to ClientsListScreen

---
+++ app/src/main/java/com/makashovadev/wifianalyzer/ui/theme/ClientsScreen.kt
@@ -0,0 +1,167 @@
+package com.makashovadev.wifianalyzer.ui.theme
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.makashovadev.wifianalyzer.domain.AccessPoint
+import com.makashovadev.wifianalyzer.domain.Client
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ClientsScreen(
+    accessPoint: AccessPoint, // сеть
+    clients: List<Client> //  клиенты этой сети
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Clients for AccessPoint id: ${accessPoint.id}")
+                },
+                navigationIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = null
+                        )
+                    }
+                }
+            )
+        }
+    )
+    { paddingsValues ->
+        LazyColumn(
+            modifier = Modifier.padding(paddingsValues),
+            contentPadding = PaddingValues(
+                top = 16.dp,
+                start = 8.dp,
+                end = 8.dp,
+                bottom = 72.dp
+            )
+        ) {
+            items(
+                items = clients,
+                key = { it.id }
+            )
+            { client ->
+                ClientItem(client = client)
+            }
+        }
+    }
+}
+
+// отображение одного клиента
+@Composable
+private fun ClientItem(
+    client: Client
+) {
+    Card(
+        modifier = Modifier
+
+            //.height(100.dp)
+            .padding(
+               // horizontal = 16.dp,
+                vertical = 4.dp
+            ),
+        border = BorderStroke(2.dp, MaterialTheme.colorScheme.secondary)
+    ) {
+        Column(
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.background)
+                .padding(
+                  //  top = 8.dp
+                )
+                .padding(
+                    top = 16.dp,
+                    start = 16.dp,
+                    end = 16.dp,
+                    bottom = 16.dp
+                )
+        ) {
+            Row(
+                modifier = Modifier
+                    .background(MaterialTheme.colorScheme.background)
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Row(
+                    modifier = Modifier
+                        .weight(1f)
+                )
+                {
+                    Text(
+                        text = client.clientAddress,
+                        style = TextStyle(
+                            color = MaterialTheme.colorScheme.primary,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 26.sp
+                        ),
+                    )
+                }
+                Row(
+                    modifier = Modifier,
+                    verticalAlignment = Alignment.CenterVertically
+                )
+                {
+                    Text(
+                        text = client.networkAddress,
+                        style = TextStyle(
+                            color = MaterialTheme.colorScheme.secondary,
+                            fontSize = 22.sp
+                        ),
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                }
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = client.clientName,
+                style = TextStyle(
+                    color = MaterialTheme.colorScheme.onSecondary,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewComment() {
+    WifiAnalyzerTheme {
+        ClientItem(client = Client(id = 0))
+    }
+}
